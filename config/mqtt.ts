b534e243097c072f1aa93726fc22d2329c60a7f4
Le Quoc Duy <117245946+duy12345567@users.noreply.github.com>
2026-04-29 23:56:45 +0700

    Add logging for MQTT connection events
    Added logging for MQTT connection status.

--- config/mqtt.ts
+++ config/mqtt.ts
@@ -1,13 +1,12 @@
 import mqtt, { MqttClient } from "mqtt";
 
 let client: MqttClient;
-
 export const connectMqtt = (): MqttClient => {
   client = mqtt.connect(`${process.env.mqtt_server}`, {
     username: `${process.env.mqtt_username}`,
     password: `${process.env.mqtt_password}`,
   });
-
+  console.log("MQTT");
   client.on("connect", () => console.log("MQTT connected"));
   client.on("error", (err) => console.error("MQTT Error:", err));
   client.on("offline", () => console.warn("MQTT Offline"));
