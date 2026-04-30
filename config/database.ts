9c05f44a31b80db326f315ecc98c01e9949befc0
Le Quoc Duy <117245946+duy12345567@users.noreply.github.com>
2026-04-30 21:34:52 +0700

    Update database

--- config/database.ts
+++ config/database.ts
@@ -4,9 +4,9 @@ export const connect = async () => {
   try {
     mongoose.connect(`${process.env.DATABASE}`)
     .then(() => 
-      console.log("Connect successfully to database")
+      console.log("Ket noi thanh cong toi database")
     );
   } catch (error) {
-    console.log("Connect fail to database");
+    console.log("Ket noi database that bai");
   }
 }
